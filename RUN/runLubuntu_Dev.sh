d61d1f139dd4dabd474b0404ad2102833c6c8ff4
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-03-02 17:20:35 +0100

    Update scripts

---
+++ RUN/runLubuntu_Dev.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+. 1-Initialize_Host_Machine.sh
+. 1-Initialize_Network-enp.sh
+. 1-Initialize_Virtualization.sh
+. 1-Initialize_Usb-Stick.sh
+. 2-Configure_Virtual_Machine-Qemu_4.2.0-Lubuntu_19.10_64-MultiUser-UsbStick.sh
+. 3-Launch_Virtual_Machine.v2.dev.sh
