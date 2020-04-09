c9ef46a6d468e469a5e8429bc22c6417de8f183c
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-04-09 20:55:39 +0200

    Add Usbkey script

--- RUN/5-Initialize_Usb-Stick.sh
+++ RUN/5-Initialize_Usb-Stick.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-lsusb
+export INFOUSBKEY=`dmesg |grep "New USB device found" |tail -1 |sed "s/.*idVendor=\(....\), idProduct=\(....\).*/\1 \2/"`
+DMESGVENDORID=`echo $INFOUSBKEY | cut -d" "  -f1`
+DMESGPRODUCTID=`echo $INFOUSBKEY | cut -d" "  -f2`
 
-HOSTBUS=002
-HOSTADDR=008
+echo $DMESGVENDORID $DMESGPRODUCTID
 
-VENDORID=23a5
-PRODUCTID=5786
+USBLIST=$(lsusb | sed "s/^Bus \(...\) Device \(...\): ID \(....\):\(....\).*/\1 \2 \3 \4/")
+while read i 
+do 
+    HOSTBUS=`echo $i | cut -d" "  -f1`
+    HOSTADDR=`echo $i | cut -d" "  -f2`
+    VENDORID=`echo $i | cut -d" "  -f3`
+    PRODUCTID=`echo $i | cut -d" "  -f4`
+    if [ "$VENDORID" == "$DMESGVENDORID" ] && [ "$PRODUCTID" == "$DMESGPRODUCTID" ] 
+    then
+        echo $HOSTBUS $HOSTADDR $VENDORID $PRODUCTID
+        break
+    fi
+done <<< "$USBLIST"
+
 
 sudo chmod 666 /dev/bus/usb/$HOSTBUS/$HOSTADDR
 
