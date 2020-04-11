9d29a86ca7e6f4241c220d575c7249a424a7ae5a
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-04-11 22:27:53 +0200

    Update Network scripts

--- RUN/runLXLE_Server1.sh
+++ RUN/runLXLE_Server1.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ ! -f ".lock" ]] 
+then
+    echo "1st install"
+    echo $installpv > .lock
+
+    . 2-Initialize_Network.sh 2
+    . 3-Initialize_Virtualization.sh
+fi
+
+echo "2nd install"
+
 . 1-Initialize_Host_Machine.sh
 . 4-Initialize_Guest_Hardware.sh 2048 2
 . 6-Configure_Virtual_Machine.sh 3 5 1 2 1 server1
