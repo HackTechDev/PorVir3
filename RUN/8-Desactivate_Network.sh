61b020d3a4cdc745c14fbcd94eedd057446256a5
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-04-11 05:26:16 +0200

    Update scripts

--- RUN/8-Desactivate_Network.sh
+++ RUN/8-Desactivate_Network.sh
@@ -6,6 +6,9 @@ case "$1" in
         sudo ip link set dev tap0 down
         sudo ip link delete tap0
 
+        sudo ip link set dev tap1 down
+        sudo ip link delete tap1
+
         sudo ip link set br0 down
         sudo ip link delete br0
         sudo ip addr add 192.168.1.3/24 dev enp2s3
