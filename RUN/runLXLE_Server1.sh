57cf75ee36cb707e48f861db81f1953516c5ebfc
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-06-23 13:14:22 +0200

    Update Server scripts

--- RUN/runLXLE_Server1.sh
+++ RUN/runLXLE_Server1.sh
@@ -12,6 +12,6 @@ fi
 echo "2nd install"
 
 . 1-Initialize_Host_Machine.sh
-. 4-Initialize_Guest_Hardware.sh 2048 2
+. 4-Initialize_Guest_Hardware.sh 1024 1
 . 6-Configure_Virtual_Machine.sh 3 5 1 2 1 server1
 . 7-Launch_Virtual_Machine.sh 2
