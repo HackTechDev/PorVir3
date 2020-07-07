9cf6bfefe93fa24762d4358fd092aba77f696ce1
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-07-07 22:29:58 +0200

    Add Scripts

---
+++ RUN/runLXLE_CubeCoin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ! -f ".lock" ]] 
+then
+    echo "1st install"
+    echo "1" > .lock
+
+    . 2-Initialize_Network.sh 2
+    . 3-Initialize_Virtualization.sh
+fi
+
+echo "2nd install"
+
+
+. ./1-Initialize_Host_Machine.sh
+. ./4-Initialize_Guest_Hardware.sh 2048 2
+. ./6-Configure_Virtual_Machine.sh 5 5 1 2 0 cubecoin
+. ./7-Launch_Virtual_Machine.sh 2
