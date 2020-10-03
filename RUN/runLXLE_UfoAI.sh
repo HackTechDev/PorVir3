73dc5ee9bdcc5cecb987841f2e3c5eef7e56538d
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-10-04 00:05:30 +0200

    Update Ufoai script

--- RUN/runLXLE_UfoAI.sh
+++ RUN/runLXLE_UfoAI.sh
@@ -14,5 +14,5 @@ echo "2nd install"
 
 . 1-Initialize_Host_Machine.sh
 . 4-Initialize_Guest_Hardware.sh 2048 2
-. 6-Configure_Virtual_Machine.sh 3 5 1 2 0 ufoai
+. 6-Configure_Virtual_Machine.sh 6 5 1 2 0 ufoai
 . 7-Launch_Virtual_Machine.sh 2
