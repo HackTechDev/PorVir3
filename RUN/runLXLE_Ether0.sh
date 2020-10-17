9b5049be6a82bd137d72e1da283ad9291e66e861
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2020-10-17 08:37:48 +0200

    Add Ether script

--- RUN/runLXLE_Ether0.sh
+++ RUN/runLXLE_Ether0.sh
@@ -13,6 +13,6 @@ echo "2nd install"
 
 
 . ./1-Initialize_Host_Machine.sh
-. ./4-Initialize_Guest_Hardware.sh 2048 2
-. ./6-Configure_Virtual_Machine.sh 6 5 1 2 0 ether
+. ./4-Initialize_Guest_Hardware.sh 1024 2
+. ./6-Configure_Virtual_Machine.sh 6 5 1 2 0 ether0
 . ./7-Launch_Virtual_Machine.sh 2
