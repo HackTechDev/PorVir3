ea12e841a7b089e44528db5b257bf9735ffd197c
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-01-18 20:45:24 +0100

    Add run scripts

---
+++ RUN/runGhostBSD_RPG.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ ! -f ".lock" ]] 
+then
+    echo "1st install"
+    echo "1" > .lock
+
+    . 2-Initialize_Network.sh 2
+    . 3-Initialize_Virtualization.sh
+fi
+
+echo "2nd install"
+
+
+
+. ./1-Initialize_Host_Machine.sh
+. ./4-Initialize_Guest_Hardware.sh 2048 2
+. ./6-Configure_Virtual_Machine.sh 6 4 4 2 0 rpg
+. ./7-Launch_Virtual_Machine.sh 2
